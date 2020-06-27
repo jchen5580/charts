d8bcc02f037a157fe5ae50f552d2a7c834d3e256
SMARTCORP\JChen <jason.chen@cioxhealth.com>
2020-06-27 14:01:24 -0400

    added publish.sh

---
+++ publish.sh
@@ -0,0 +1,17 @@
+echo 'package all charts in '$(pwd)/$1
+for dir in $1/*/; do
+    echo 'packaging chart in '${dir}
+    helm package $dir
+done
+
+echo 'clean up dist folder'
+rm dist/*.*
+
+echo 'move packed charts to dist'
+mv *.tgz dist/
+
+echo 'update index of charts in repo https://jchen5580.github.io/charts'
+helm repo index dist/ --url https://jchen5580.github.io/charts
+
+echo 'push charts in dist/ to repo https://jchen5580.github.io/charts'
+helm repo add dist/ https://jchen5580.github.io/charts
